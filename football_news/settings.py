4a4c43fda631c51cb6574f79be8fd69e060c1855
unknown <guarddin29@gmail.com>
2025-08-27 13:33:15 +0700

    Add URL in settings.py

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = 'django-insecure-(#qx-vlo_p#7g5nusgdm=0m_1_k*hnb*yx5q=b)xw(*%iirku(
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+# Aslinya neal.guarddin menjadi neal-guarddin
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "neal-guarddin-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
